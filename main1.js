6ed3d8def92e1c84d4af680a645f1adf01a2bd38
alexoviedo <oviedo.alex@gmail.com>
2017-07-05 15:08:28 -0400

    JS first assignment first commit

---
+++ main1.js
@@ -0,0 +1,21 @@
+// let born = 1984;
+// let today = 2017;
+// let timAge = (today-born);
+// // A:
+// console.log("Tim is " + timAge +" years old");
+//
+// let x = "true";
+//
+// console.log(typeof x);
+//
+// let r = (NaN === NaN);
+//
+// console.log(r)
+
+let x = 1 * undefined;
+
+if(x){
+	console.log("True!");
+} else {
+	console.log("False!");
+}
